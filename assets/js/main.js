b52754f983da2b87c051072e1c947a895d6b2e49
Prabin Magar <dev7@esignature.com.np>
2025-08-01 15:55:55 +0545

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let swiperOurClients;
   const clientsSwiperContainer = document.querySelector('.home-clients-slider');
+  const clientsReviewsSwiperContainers = document.querySelectorAll('.tab-pane-slider');
 
+  // Initialize client logo slider
   if (clientsSwiperContainer) {
-    swiperOurClients = new Swiper(clientsSwiperContainer, {
+    const clientSwiper = new Swiper(clientsSwiperContainer, {
       loop: true,
       slidesPerView: "auto",
       freeMode: true,
-      // centeredSlides: true,
       spaceBetween: 20,
       autoplay: {
         delay: 3000,
+        disableOnInteraction: false,
       },
       observer: true,
       observeParents: true,
     });
+
+    clientSwiper.update(); // optional unless dynamic changes happen
   }
 
-  if (swiperOurClients) {
-    swiperOurClients.update();
-  }
+  // Initialize each review slider
+  clientsReviewsSwiperContainers.forEach((container) => {
+    new Swiper(container, {
+      loop: true,
+      spaceBetween: 0,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      slidesPerView: 1, 
+      breakpoints: {
+        576: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        }
+      },
+      observer: true,
+      observeParents: true,
+      pagination: {
+        el: container.querySelector(".swiper-pagination"),
+        clickable: true,
+      },
+    });
+  });
 });
