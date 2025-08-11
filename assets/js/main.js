18234eea81794e5e9dc8041d99208172732429a3
Prabin Magar <dev7@esignature.com.np>
2025-08-11 09:50:02 +0545

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,70 @@ document.addEventListener("DOMContentLoaded", () => {
   const screenshotsProductSwiperContainer = document.querySelector('.screenshots-product-slider');
   const projectsBtnsSwiperContainer = document.querySelector('.projects-btns-slider');
   const projectsBtnsSwiperContainerWrapper = document.querySelector('.projects-btns-slider-wrapper');
+  const appscreensSwiperContainer = document.querySelector('.appscreens-slider');
+
+  document.querySelectorAll('.atech-menu-item').forEach(item => {
+    const submenu = item.querySelector('.atech-menu-submenu');
+
+    if (submenu) {
+      item.addEventListener('mouseenter', () => {
+        submenu.style.display = 'block';
+        submenu.style.opacity = '1';
+        submenu.style.visibility = 'visible';
+      });
+
+      item.addEventListener('mouseleave', () => {
+        submenu.style.display = 'none';
+        submenu.style.opacity = '0';
+        submenu.style.visibility = 'hidden';
+      });
+    }
+  });
+
+
+  const menuLinks = document.querySelectorAll('.atech-menu-link');
+
+  menuLinks.forEach(link => {
+    const submenuClass = link.getAttribute('data-submenu');
+    const submenu = document.querySelector(`.${submenuClass}`);
+
+    if (submenu) {
+      let timeout;
+
+      link.addEventListener('mouseenter', () => {
+        clearTimeout(timeout);
+        // submenu.style.display = 'block';
+        submenu.style.opacity = '1';
+        submenu.style.visibility = 'visible';
+      });
+
+      // Start hide timer when leaving link
+      link.addEventListener('mouseleave', () => {
+        timeout = setTimeout(() => {
+          // submenu.style.display = 'none';
+          submenu.style.opacity = '0';
+          submenu.style.visibility = 'hidden';
+        }, 200);
+      });
+
+      // Keep submenu open when hovered
+      submenu.addEventListener('mouseenter', () => {
+        clearTimeout(timeout);
+        // submenu.style.display = 'block';
+        submenu.style.opacity = '1';
+        submenu.style.visibility = 'visible';
+      });
+
+      // Hide submenu when leaving
+      submenu.addEventListener('mouseleave', () => {
+        timeout = setTimeout(() => {
+          // submenu.style.display = 'none';
+          submenu.style.opacity = '0';
+          submenu.style.visibility = 'hidden';
+        }, 200);
+      });
+    }
+  });
 
   // Initialize client logo slider
   if (clientsSwiperContainer) {
@@ -140,96 +204,36 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 
-  document.querySelectorAll('.atech-menu-item').forEach(item => {
-    const submenu = item.querySelector('.atech-menu-submenu');
-
-    if (submenu) {
-      item.addEventListener('mouseenter', () => {
-        submenu.style.display = 'block';
-        submenu.style.opacity = '1';
-        submenu.style.visibility = 'visible';
-      });
-
-      item.addEventListener('mouseleave', () => {
-        submenu.style.display = 'none';
-        submenu.style.opacity = '0';
-        submenu.style.visibility = 'hidden';
-      });
-    }
-  });
-
-
-  const menuLinks = document.querySelectorAll('.atech-menu-link');
-
-  menuLinks.forEach(link => {
-    const submenuClass = link.getAttribute('data-submenu');
-    const submenu = document.querySelector(`.${submenuClass}`);
-
-    if (submenu) {
-      let timeout;
-
-      link.addEventListener('mouseenter', () => {
-        clearTimeout(timeout);
-        // submenu.style.display = 'block';
-        submenu.style.opacity = '1';
-        submenu.style.visibility = 'visible';
-      });
-
-      // Start hide timer when leaving link
-      link.addEventListener('mouseleave', () => {
-        timeout = setTimeout(() => {
-          // submenu.style.display = 'none';
-          submenu.style.opacity = '0';
-          submenu.style.visibility = 'hidden';
-        }, 200);
-      });
-
-      // Keep submenu open when hovered
-      submenu.addEventListener('mouseenter', () => {
-        clearTimeout(timeout);
-        // submenu.style.display = 'block';
-        submenu.style.opacity = '1';
-        submenu.style.visibility = 'visible';
-      });
-
-      // Hide submenu when leaving
-      submenu.addEventListener('mouseleave', () => {
-        timeout = setTimeout(() => {
-          // submenu.style.display = 'none';
-          submenu.style.opacity = '0';
-          submenu.style.visibility = 'hidden';
-        }, 200);
-      });
-    }
-  });
-
-
-  // Initialize Isotope
-  var iso = new Isotope('.projects-listing-grid', {
-    itemSelector: '.projects-grid-item-wrapper',
-    layoutMode: 'fitRows'
-  });
-
-  // Filter buttons group
-  var filtersElem = document.querySelector('.filters-button-group');
-  var filterButtons = filtersElem.querySelectorAll('button');
-
-  filtersElem.addEventListener('click', function (event) {
-    var target = event.target.closest('button');
-    if (!target) return;
-
-    // Get filter value
-    var filterValue = target.getAttribute('data-filter');
-    iso.arrange({ filter: filterValue });
-
-    // Remove .is-checked from all buttons
-    filterButtons.forEach(function (btn) {
-      btn.classList.remove('is-checked');
+  if (appscreensSwiperContainer) {
+    new Swiper(appscreensSwiperContainer, {
+      loop: true,
+      speed: 1000,
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+      },
+      keyboard: {
+        enabled: true
+      },
+      observer: true,
+      observeParents: true,
+      pagination: {
+        el: appscreensSwiperContainer.querySelector(".swiper-pagination"),
+        clickable: true,
+      },
     });
-
-    // Add .is-checked to clicked button
-    target.classList.add('is-checked');
-  });
+  }
 });
 
 
